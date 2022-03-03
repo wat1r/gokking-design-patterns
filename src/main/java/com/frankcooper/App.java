0d61f6d7595f81288b249db6e763d227b3a9102e
wangzhou <wangzhou@shengqugames.com>
2022-03-03 18:20:08 +0800

    init

---
+++ src/main/java/com/frankcooper/App.java
@@ -0,0 +1,17 @@
+package com.frankcooper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+
+        System.out.println();
+
+    }
+}
