b768e4cb142e7530a7262d779aac2649db1a2c65
wangzhou <wangzhou@shengqugames.com>
2022-03-03 19:19:10 +0800

    init 1

---
+++ src/main/java/com/frankcooper/desgin/pattern/factory/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author: wangzhou(Frank Cooper)
+ * @date: 2022/3/3 19:18
+ * @description:
+ */
+package com.frankcooper.desgin.pattern.factory;
---
+++ src/main/java/com/frankcooper/design/pattern/One.java
@@ -0,0 +1,9 @@
+package com.frankcooper.design.pattern;
+
+/**
+ * @author: wangzhou(Frank Cooper)
+ * @date: 2022/3/3 19:18
+ * @description:
+ */
+public class One {
+}
